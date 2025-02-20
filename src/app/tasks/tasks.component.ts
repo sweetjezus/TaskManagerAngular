f3faa135ed2332bd5d3532521c53e64abd68e66e
sweetjezus <144045892+sweetjezus@users.noreply.github.com>
2025-02-20 17:16:47 +0000

    Modules
    Changed to a Module approach

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,13 +1,8 @@
 import { Component, Input } from '@angular/core';
-import { TaskComponent } from './task/task.component';
-import { NewTaskComponent } from './new-task/new-task.component';
-import { type NewTaskData } from './task/task.model';
 import { TaskService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
-  standalone: true,
-  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
@@ -22,8 +17,6 @@ export class TasksComponent {
   get selectedUserTasks() {
     return this.tasksService.getUserTasks(this.userId);
   }
-
-  onCompleteTask(id: string) {}
 
   onStartAddTask() {
     this.isAddingTask = true;
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,17 +1,12 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { User } from './user.model';
-import { CardComponent } from '../shared/card/card.component';
 
 @Component({
   selector: 'app-user',
-  standalone: true,
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
-  imports: [CardComponent],
 })
 export class UserComponent {
-  // ! to tell typescript that it will be set to some value
-  // { required: true } to indicate that the value is required in the app.component or wherever the app-user is used~
   @Input({ required: true }) user!: User;
   @Input({ required: true }) selected!: boolean;
   @Output() select = new EventEmitter<string>();
